d1f277eb481675b42de48b4cc9719fca92ac4473
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-06-30 21:42:34 +0530

    3028

--- leetcode/3028.cpp
+++ leetcode/3028.cpp
@@ -1,26 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int returnToBoundaryCount(vector<int>& nums) 
-{
+int returnToBoundaryCount(vector<int>& nums) {
     int antPos=0;
     int count=0;
-    for(int i=0;i<nums.size();i++)
-    {
-        antPos += nums[i]; 
+    for(int i=0;i<nums.size();i++) {
+        antPos += nums[i];
+        if(antPos==0) {
+            count++;
+        }   
     }
-    if(antPos==0)
-    {
-        cout << "1";
-        return 1;
-    }
-    else
-        cout << "0";
-        return 0;
+    return count;
 }
-int main()
-{
-    vector<int> myVector ={2,3,-5};
+int main() {
+    vector<int> myVector ={2,3,-5,5,-5};
     int output = returnToBoundaryCount(myVector);
-    cout << endl;
-    cout << output;
+    cout << "result: " << output;
 }
