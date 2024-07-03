60d031e7fbc76b3061d42e8d0e09aabbfff4d81c
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-03 19:29:58 +0530

    var

--- leetcode/1450.cpp
+++ leetcode/1450.cpp
@@ -4,10 +4,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-		// Declare answer variable
         int ans = 0;
-		
-		// constantly check every interval if it contains queryTime
         for (int i = 0; i < startTime.size(); i++)
             if (startTime[i] <= queryTime && endTime[i] >= queryTime)
                 ans++;
