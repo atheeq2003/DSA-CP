30567788f0b96a1ecd372c849d4ca705b04c390b
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-03 16:17:02 +0530

    1464

---
+++ leetcode/1464.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxProduct(vector<int>& nums) {
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+
+        for (int i=0; i<nums.size(); i++) {
+            int num = nums[i];
+            if (num >= max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+int main() {
+    vector<int> myVector ={3,4,5,2};
+    int output = maxProduct(myVector);
+    cout << "result: " << output;
+}
