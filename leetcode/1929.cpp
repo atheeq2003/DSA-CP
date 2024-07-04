b0715f63525c743ff1c962274588c75ad4aaaec1
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-04 20:23:50 +0530

    1929

---
+++ leetcode/1929.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> getConcatenation(vector<int>& nums) {
+    int n=nums.size();
+    vector<int> ans(2*n);
+    for(int i=0;i<n;i++) {
+        ans[i] = nums[i];
+        ans[i+n] = nums[i];
+    }
+    return ans;
+}
+int main() {
+    vector<int> myVector = {1,2,1};
+    vector<int> output = getConcatenation(myVector);
+    for(int i=0;i<output.size();i++) {
+        cout << output[i] << " ";
+    }
+}
