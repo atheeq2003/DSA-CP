a7e882524996b99bb7cb723bc4173f5773b5db18
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-04 21:42:05 +0530

    3024

---
+++ leetcode/3024.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+string triangleType(vector<int>& nums) {
+    if(!((nums[0]+nums[1]>nums[2]) && (nums[0]+nums[2]>nums[1]) && (nums[1]+nums[2]>nums[0])))
+        return "none";
+    if(nums[0] == nums[1] && nums[1] == nums[2])
+        return "equilateral";
+    else if(nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2])
+        return "scalene";
+    return "isosceles";
+}
+int main() {
+    vector<int> myVector={3,4,5};
+    string output = triangleType(myVector);
+    cout << output << endl;
+}
