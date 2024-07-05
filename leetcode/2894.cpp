808390c909962f8eb71987d67589645775324bab
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-05 21:01:46 +0530

    2894

---
+++ leetcode/2894.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int differenceOfSums(int n, int m) {
+    int num1, num2;
+    num1 = n*(n+1)/2;
+    num2 = m*(1+(n/m))*(n/m);
+    int result = num1-num2;
+    return result;
+}
+int main(){
+    int output = differenceOfSums(10,3);
+    cout << output;
+}
