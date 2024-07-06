0eb009f15d5a9efe59965cf36979bc9d63416a7b
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-06 21:26:30 +0530

    1281

---
+++ leetcode/1281.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int subtractProductAndSum(int n) {
+    string str;
+    str = to_string(n);
+    int product=1;
+    int sum=0;
+    for(int i=0;i<str.size();i++) {
+        int digits=str[i]-'0';
+        product *= digits;
+        sum += digits;   
+    }
+    return product-sum;
+}
+int main() {
+    int output = subtractProductAndSum(234);
+    cout << output;
+}
