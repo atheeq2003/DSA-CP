2922a9a277209c5455c1ea72d0b9f65934fd20b2
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-08 21:58:42 +0530

    1518

---
+++ leetcode/1518.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numWaterBottles(int numBottles, int numExchange) {
+    int totalBottles = numBottles;
+    while (numBottles>=numExchange) {
+        totalBottles+=numBottles / numExchange;
+        numBottles=(numBottles / numExchange) + (numBottles % numExchange);
+    }
+    return totalBottles;
+}
+int main() {
+    int output = numWaterBottles(9,3);
+    cout << output;
+}
