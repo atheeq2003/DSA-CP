08c3ed31b79bf4ddb8b1a39200a1d59bc13e5794
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-10 13:44:57 +0530

    refactor

--- leetcode/2413.cpp
+++ leetcode/2413.cpp
@@ -1,3 +1,6 @@
+// 2413 - Smallest Even Multiple
+// Easy - Math, Number Theory
+// https://leetcode.com/problems/smallest-even-multiple/description/
 #include<bits/stdc++.h>
 using namespace std;
 int smallestEvenMultiple(int n) {
