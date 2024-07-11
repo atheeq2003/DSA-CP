e128467f06f8f886b2e5affe7000324f738a6898
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-11 23:43:14 +0530

    Ex2

---
+++ HackerRank/C++/Ex2.cpp
@@ -0,0 +1,11 @@
+// Input and Output
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    int sum=a+b+c;
+    cout << sum;
+    return 0;
+}
