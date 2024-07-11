b449615f6ef44fc2907d51723d8048092a1afc5a
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-11 23:12:39 +0530

    Ex1

---
+++ HackerRank/Ex1.cpp
@@ -0,0 +1,8 @@
+// Say "Hello, World!" With C++
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
