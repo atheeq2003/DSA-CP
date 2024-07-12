86511d400c28b68e4f695b1d665e850b301e762a
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-12 21:36:22 +0530

    2469

---
+++ leetcode/2469.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<double> convertTemperature(double celsius) {
+    vector<double> ans;
+    double kelvin=0;
+    double fahrenheit=0;
+    kelvin = celsius + 273.15;
+    fahrenheit = celsius * 1.80 + 32.00;
+    ans.push_back(kelvin);
+    ans.push_back(fahrenheit);
+    return ans;
+}
+int main() {
+    vector<double> output = convertTemperature(36.50);
+    for(int i=0;i<=output.size();i++) {
+        cout << output[i] << " ";
+    }
+}
