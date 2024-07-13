48685eb06d4b551d6e3775ead64e92afd5f9f717
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-13 22:14:38 +0530

    1502

---
+++ leetcode/1502.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxPower(string s) {
+    int count=1;
+    int maxim=1;
+    for(int i=1;i<s.size();i++) {
+        if(s[i]==s[i-1]){
+            count++;
+        }
+        else{
+            count=1;
+        }
+        maxim = max(maxim,count);
+    }
+    return maxim;
+}
+int main() {
+    int output = maxPower("leetcode");
+    cout << output << endl;
+}
