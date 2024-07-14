06777e97c9a990314c16dfdb6fa7e25a332249da
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-14 20:40:20 +0530

    Ex11

---
+++ HackerRank/C++/Ex11.cpp
@@ -0,0 +1,39 @@
+// Variable Sized Arrays
+// https://www.hackerrank.com/challenges/variable-sized-arrays/problem?isFullScreen=true
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main() {
+    int n, q;
+    cin >> n >> q;
+    cin.ignore();
+    vector<vector<int>> a(n);
+
+    for (int i = 0; i < n; i++) {
+        string line;
+        getline(cin, line);
+        istringstream ss(line);
+        int k_size, k_item;
+        ss >> k_size;
+        vector<int> k(k_size, 0);
+        for (int j = 0; j < k_size; j++) {
+            ss >> k_item;
+            k[j] = k_item;
+        }
+        a[i] = k;
+    }
+
+    for (int i = 0; i < q; i++) {
+        string query;
+        getline(cin, query);
+        istringstream ss(query);
+        int x, y;
+        ss >> x >> y;
+        cout << a[x][y] << endl;
+    }
+       
+    return 0;
+}
