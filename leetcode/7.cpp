52bc4025b1c4e9759e3798709b1f5281116c587e
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-15 21:04:42 +0530

    Refactor

--- leetcode/7.cpp
+++ leetcode/7.cpp
@@ -1,5 +1,10 @@
+// 7 - Reverse Integer
+// Medium - Math
+// https://leetcode.com/problems/reverse-integer/description/
 #include<bits/stdc++.h>
 using namespace std;
+// Initial Approach 
+/*
 int reverse(int x) {
     int revNum=0;
     bool isNegative=false;
@@ -23,6 +28,20 @@ int reverse(int x) {
     }
     return revNum;
 }
+*/
+// Works in LC
+int reverse(int x) {
+    int y=0;
+        while(x){
+            if(y>INT_MAX/10 || y<INT_MIN/10){
+                return 0;
+            }else{
+                y=y*10 +x%10;
+                x=x/10;
+            }
+        }
+        return y;
+    }
 int main() {
     int output = reverse(-2147483648);
     cout << output;
