7cefa80cc9b6983d88ccc69db2336f914cf77ae8
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-15 17:53:11 +0530

    vector file tweak

--- learning/STL/vector.cpp
+++ learning/STL/vector.cpp
@@ -7,10 +7,12 @@ void explainVector() {
     vector<pair<int,int>> vec;
     vec.push_back({2,4});
     vec.emplace_back(4,6);
+    // (5,100) -> 5 size of vector and 100 is the value that will be stored in 5 indices
     vector<int> v1(5,100);
     vector<int> v2(5,10);
     v2.push_back(6);
     vector<int> v3(v1);
+    cout << v3[0] << endl;
 }
 int main() {
     explainVector();
