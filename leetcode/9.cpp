ca70c762315c6b7a6a27cad393b75a4cea514ba1
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-15 21:49:01 +0530

    9th

---
+++ leetcode/9.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalindrome(int x) {
+    long long revX=0;
+    long long n=x;
+    if(x>=0){
+        while(x) {
+            int lastDigit=x%10;
+            x=x/10;
+            revX=revX*10+lastDigit;
+        }
+    }
+    else
+        return false; 
+    if(revX==n)
+        return true;
+    else
+        return false;
+}
+int main() {
+    bool output = isPalindrome(121);
+    cout << output;
+}
