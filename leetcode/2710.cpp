bae607dc608076d6270e4c86b6b475d33f6bb46b
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-17 22:18:34 +0530

    2710

---
+++ leetcode/2710.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+string removeTrailingZeros(string num) {
+    int n = num.length();
+    int i;
+    for(i = n - 1; i >= 0; i--){
+        if(num[i] != '0') break;
+    }
+    return num.substr(0,i + 1);
+}
+int main() {
+    string output = removeTrailingZeros("51230100");
+    cout << output;
+}
