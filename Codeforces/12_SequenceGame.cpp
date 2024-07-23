aed25bc8055af0d2f262806d6861dd64a36194d4
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-23 20:27:52 +0530

    Sequence Game

---
+++ Codeforces/12_SequenceGame.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(auto &i : arr) {
+            cin >> i;
+        }
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i=1; i<n; i++) {
+            if(arr[i] >= arr[i-1]) {
+                ans.push_back(arr[i]);
+            }
+            else {
+                ans.push_back(arr[i]);
+                ans.push_back(arr[i]);
+            }
+        }
+        cout << ans.size() << "\n";
+        for(auto x : ans) {
+            cout << x << " ";
+        }
+    }
+}
