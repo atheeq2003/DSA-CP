1059b476a9daed3572a5a948aeb4e6e3cce596c8
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-24 18:34:21 +0530

    Array Coloring

---
+++ Codeforces/15_ArrayColoring.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(auto &i : arr) {
+            cin >> i;
+        }
+        int sum=0;
+        for(auto x : arr) {
+            sum+=x;
+        }
+        if(sum%2 == 0) {
+            cout << "YES" << "\n";
+        }
+        else {
+            cout << "NO" << "\n";
+        }
+    }
+}
