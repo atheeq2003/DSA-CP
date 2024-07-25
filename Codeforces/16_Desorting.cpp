080ee141b45acfd8cfa93969daade48a75e28ab2
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-25 20:48:21 +0530

    Refactor

--- Codeforces/16_Desorting.cpp
+++ Codeforces/16_Desorting.cpp
@@ -1,3 +1,6 @@
+// 800  - A
+// 1853 - Desorting (brute force, greedy, math)
+// https://codeforces.com/problemset/problem/1853/A
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
@@ -13,11 +16,13 @@ int main() {
         if(!is_sorted(arr.begin(), arr.end())) {
             cout << 0 << "\n";
         }
-        int diff = INT_MAX;
-        for(int i=1; i<n; i++) {
-            diff = min(diff, arr[i] - arr[i-1]);
+        else {
+            int diff = INT_MAX;
+            for(int i=1; i<n; i++) {
+                diff = min(diff, arr[i] - arr[i-1]);
+            }
+            int ops = diff/2 + 1;
+            cout << ops << "\n";
         }
-        int ops = diff/2 + 1;
-        cout << ops << "\n";
     }
 }
