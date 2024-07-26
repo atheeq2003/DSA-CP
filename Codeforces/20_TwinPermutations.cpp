8d6aa84551a191757b751cabf5e8cab2ba288566
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-26 22:41:49 +0530

    Twin Permutations

---
+++ Codeforces/20_TwinPermutations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(auto &i : arr) {
+            cin >> i;
+        }
+        for(int i=0; i<n; i++) {
+            cout << n - arr[i] + 1 << " ";
+        }
+        cout << "\n";
+    }
+}
