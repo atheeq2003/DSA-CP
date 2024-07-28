d87273e4e8b6b67899faf26afb3807443e97f8f6
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-07-28 21:15:15 +0530

    Prepend and Append

---
+++ Codeforces/25_PrependAndAppend.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n; 
+        cin >> n;
+        string s;
+        cin >> s;
+        int i=0, j=n-1;
+        while(i<j && s[i] != s[j]) {
+            i++;
+            j--;
+        }
+        cout << j-i+1 << "\n";
+    }
+}
