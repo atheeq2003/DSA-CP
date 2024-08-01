34110ca9c34b243a13b68a7942a264a4ecad2c72
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-01 19:51:01 +0530

    Everybody Likes Good Arrays

---
+++ Codeforces/29_EverybodyLikesGoodArrays.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int count = 0;
+        vector<int> arr(n);
+        for(auto &i : arr) {
+            cin >> i;
+        }
+        for(int i=0; i<n-1; i++) {
+            count+=(arr[i]%2 == arr[i+1]%2);
+        }
+        cout << count << "\n";
+    }
+}
