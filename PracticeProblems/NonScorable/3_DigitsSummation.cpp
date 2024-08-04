6f1be99962cbcc4f98bea7ed50b5b9ae4caccd18
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-04 17:37:10 +0530

    Digits Summation

---
+++ PracticeProblems/NonScorable/3_DigitsSummation.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/my
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long n,m;
+    cin >> n >> m;
+    long long ans = 0;
+    ans = n % 10 + m % 10;
+    cout << ans << endl;
+}
