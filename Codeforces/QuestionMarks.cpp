6236ff9c0a004248685f454012ba3310295a6020
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-05 12:31:15 +0530

    Question Marks

---
+++ Codeforces/QuestionMarks.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int countA=0;
+        int countB=0;
+        int countC=0;
+        int countD=0;
+        int questionMarks=0;
+        for(int i=0; i<4*n; i++) {
+            if(s[i] == 'A' && countA < n) {
+                countA++;
+            }
+            else if(s[i] == 'B' && countB < n) {
+                countB++;
+            }
+            else if(s[i] == 'C' && countC < n) {
+                countC++;
+            }
+            else if(s[i] == 'D' && countD < n) {
+                countD++;
+            }
+            else if(s[i] == '?') {
+                questionMarks++;
+            }
+        }
+        cout << countA+countB+countC+countD << endl;
+    }
+}
