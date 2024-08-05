f5a6129f0545f822dcc686f553029a5d3eea38dc
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-05 15:15:02 +0530

    Watermelon

---
+++ PracticeProblems/NonScorable/9_Watermelon.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/4/A
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int w;
+    cin >> w;
+    if(w == 2) {
+        cout << "NO" << "\n";
+    }
+    else if(w % 2 == 0) {
+        cout << "YES" << "\n";
+    }
+    else {
+        cout << "NO" << "\n"; 
+    }
+}
