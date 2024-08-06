0366ea93ba01a32d9c259367acab875d04989ba9
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-06 19:59:47 +0530

    Print Names

--- DSA-Learning/3_Recursion/2_PrintNames.cpp
+++ DSA-Learning/3_Recursion/2_PrintNames.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printNos(int cnt, int n) {
+void printNames(int cnt, int n) {
     if(cnt > n) {
         return;
-    } 
-    cout << cnt << " ";
-    printNos(cnt+1, n);
+    }
+    cout << "Atheeq" << " ";
+    printNames(cnt+1, n);
 }
 int main() {
     int n;
     cin >> n;
-    printNos(1,n);
+    printNames(1, n);
 }
