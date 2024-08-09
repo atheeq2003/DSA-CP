6d21d8854532d2c3d20ffa769beb6633fc88e2ec
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-09 23:38:45 +0530

    Slavic's Exam

---
+++ Codeforces/Slavic'sExam.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        string s, t;
+        cin >> s >> t;
+        int n = s.size();
+        int m = t.size();
+        int i = 0;
+        int j = 0;
+        while (i < n && j < m)
+        {
+            if (s[i] == '?')
+            {
+                s[i] = t[j];
+                i++;
+                j++;
+            }
+            else
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        if (j == m)
+        {
+            cout << "YES" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] != '?')
+                {
+                    cout << s[i];
+                }
+                else
+                {
+                    cout << 'a';
+                }
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
