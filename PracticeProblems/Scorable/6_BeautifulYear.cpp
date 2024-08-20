8ed3d70ced44f5d997ab95ac7d000a07cef084b1
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-08-21 00:02:20 +0530

    Again 25

--- PracticeProblems/Scorable/6_BeautifulYear.cpp
+++ PracticeProblems/Scorable/6_BeautifulYear.cpp
@@ -5,12 +5,12 @@ int main() {
   int y;
   cin >> y;
   for(int i=y+1; i<10000; i++) {
-      string s=to_string(i);
-        vector<int> arr(10);
-        int oneCount=0;
+    string s=to_string(i);
+    vector<int> arr(10);
+    int oneCount=0;
     for(int j=0; j<4; j++) {
         int val=s[j]-'0'; 
-          arr[val]++;
+        arr[val]++;
     }
     for(int j=0; j<10; j++) {
       if(arr[j] == 1) {
---
+++ PracticeProblems/Scorable/7_Again25.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/630/A
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  cin >> n;
+  if(n>=2) {
+    cout << 25;
+  }
+  else {
+    cout << 5;
+  }
+}
