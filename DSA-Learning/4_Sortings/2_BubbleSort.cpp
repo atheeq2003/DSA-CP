d3315266682ca66123a948bd3edc44442c304b21
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-09-05 20:19:26 +0530

    Refactor

--- DSA-Learning/4_Sortings/2_BubbleSort.cpp
+++ DSA-Learning/4_Sortings/2_BubbleSort.cpp
@@ -1,10 +1,12 @@
+// Worst Case and Average Case Time Complexity - O(n^2)
+// Best Case - O(n) and that is achieved by not doing additional unnecessary checks for already sorted using bool flag.
 #include<bits/stdc++.h>
 using namespace std;
 vector<int> bubble_sort(vector<int> arr, int n) {
     bool swapped = false;
     for(int i=0; i<n-1; i++) {
         for(int j=0; j<n-1-i; j++) {
-            if(arr[j] > arr[j+1]) { // 54321
+            if(arr[j] > arr[j+1]) {
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
