e0beaa6e9ea0a0f9ebefe03e4c878070d5aea7df
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-05 15:45:57 +0530

    Largest Element in an Array

---
+++ DSA-Learning/5_Arrays/1_LargestElementInArray.cpp
@@ -0,0 +1,42 @@
+// Largest Element in an Array
+#include<bits/stdc++.h>
+using namespace std;
+
+/* 
+Brute Force Approach:
+    Time Complexity: O(N*log(N))
+    Space Complexity: O(n) 
+*/
+int largestElementBrute(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    int ans = arr[arr.size()-1];
+    return ans;
+}
+
+
+/* 
+Optimised Approach:
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+*/
+int largestElementOptimised(vector<int> arr) {
+    int max = arr[0];
+    for(int i=0; i<arr.size(); i++) {
+        if(max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<arr.size(); i++) {
+        cin >> arr[i];
+    }
+    cout << largestElementBrute(arr) << "\n";
+    cout << largestElementOptimised(arr);
+}
