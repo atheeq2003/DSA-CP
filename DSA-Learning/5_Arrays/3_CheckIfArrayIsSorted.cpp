570355b109f9fa3785036bdda5cf206f62e2e08b
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-07 17:09:18 +0530

    Refactor

--- DSA-Learning/5_Arrays/3_CheckIfArrayIsSorted.cpp
+++ DSA-Learning/5_Arrays/3_CheckIfArrayIsSorted.cpp
@@ -1,12 +1,16 @@
+// Check if array is sorted and rotated
 #include<bits/stdc++.h>
 using namespace std;
 bool check(vector<int>& nums) {
     int count = 0;
     size_t n = nums.size();
-    for (size_t i = 0; i < n; i++) {
-        if (nums[i] > nums[(i + 1) % n]) {
+    for (size_t i = 0; i < n-1; i++) {
+        if (nums[i] > nums[i+1]) {
             count++;
         }
+    }
+    if(nums[n-1] > nums[0]) {
+        count++;
     }
     if(count <= 1) {
         return true;
