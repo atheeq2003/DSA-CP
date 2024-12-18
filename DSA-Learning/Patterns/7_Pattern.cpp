179835eb5af8e094da4e4fd3d00d0dda12388927
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-18 20:02:19 +0530

    pattern 6 and 7

---
+++ DSA-Learning/Patterns/7_Pattern.cpp
@@ -0,0 +1,20 @@
+/*
+    1
+    2 2
+    3 3 3
+    4 4 4 4
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int value = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i+1; j++) {
+            cout << value << " ";
+        }
+        value++;
+        cout << endl;
+    }
+}
