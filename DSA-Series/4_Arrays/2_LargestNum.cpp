92f935cf96ddd96e41c7c47997b5f0ef51ec3578
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-27 16:03:43 +0530

    Largest Num

---
+++ DSA-Series/4_Arrays/2_LargestNum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largeNum(int arr[], int n) {
+    int large = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > large) {
+            large = arr[i];
+        }
+    }
+    return large;
+}
+
+int main() {
+    int large = INT_MIN;
+    int arr[] = {10, -15, 35, 0, 90, 100};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << largeNum(arr, n) << endl;
+    for(int i = 0; i < n; i++) {
+        large = max(arr[i], large);
+    }
+    cout << large << endl;
+}
