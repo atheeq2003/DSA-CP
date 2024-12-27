1d6168817a76dd1a3d1b454b114011da04a0fa32
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-27 21:53:53 +0530

    Reverse Array

---
+++ DSA-Series/4_Arrays/4_ReverseArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void revArray(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Original Array: " << endl;
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    revArray(arr, n);
+    cout << "Reversed Array: " << endl;
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
