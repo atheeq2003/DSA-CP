a8585147d656e931db9f6c1ace49d0e5923e6234
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-01-06 20:29:12 +0530

    Buy and Sell

---
+++ DSA-Series/6_Vector_2/6_BuyAndSellStock.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int buySellStock(vector<int> arr) {
+    // Stores the profit of price that can be sold when the stock value is high [arr[i] - BestpriceToBuy]
+    int maxProfit = 0;
+    // The best price to buy the stock
+    int bestPriceToBuy = arr[0];
+    for(int i = 1; i < arr.size(); i++) {
+        bestPriceToBuy = min(bestPriceToBuy, arr[i]);
+        if(arr[i] > bestPriceToBuy) {
+            maxProfit = max(maxProfit, arr[i] - bestPriceToBuy);
+        }
+    }
+    return maxProfit;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    cout << buySellStock(arr);
+}
