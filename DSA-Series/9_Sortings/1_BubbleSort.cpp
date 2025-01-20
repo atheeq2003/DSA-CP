94e4c6ac3a9737cb545be773f65361d590e02899
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-01-20 20:13:44 +0530

    Bubble Sort

---
+++ DSA-Series/9_Sortings/1_BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int> &arr) {
+    int n = arr.size();
+    for(int i = 0; i < n-1; i++) {
+        bool isSwap = false;
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                /*  int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;   
+                */
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int &i : arr) {
+        cin >> i;
+    }
+    bubbleSort(arr);
+    for(auto i : arr) {
+        cout << i << " ";
+    }
+}
