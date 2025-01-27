d4a4cd5fe50abb9f655a0fd2503f3aa5164f5690
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-01-27 14:30:28 +0530

    Rotate Array

--- DSA-Series/5_Vectors_1/5_RightRotateArray.cpp
+++ DSA-Series/5_Vectors_1/5_RightRotateArray.cpp
@@ -4,29 +4,31 @@ using namespace std;
 
 void rotateOptimal(vector<int> &arr, int k) {
     int n = arr.size();
-    reverse(arr.begin(), arr.begin() + n - k);
-    reverse(arr.begin() + n - k, arr.end());
+    k = k % n;
+    reverse(arr.begin(), arr.begin()+(n-k));
+    reverse(arr.begin()+(n-k), arr.end());
     reverse(arr.begin(), arr.end());
 }
 
-void rotateBrute(vector<int> &arr, int &k) {
-    k = k % arr.size();
+void rotateBrute(vector<int> &arr, int k) {
+    int n = arr.size();
+    k = k % n;
     vector<int> temp(k);
+    for(int i = n-k; i < n; i++) {
+        temp[i-(n-k)] = arr[i];
+    }
+    for(int i = n-k-1; i >= 0; i--) {
+        arr[i+k] = arr[i];
+    }
     for(int i = 0; i < k; i++) {
-        temp[i] = arr[i];
-    }
-    for(int i = k; i < arr.size(); i++) {
-        arr[i-k] = arr[i];
-    }
-    for(int i = arr.size() - k; i < arr.size(); i++) {
-        arr[i] = temp[i-(arr.size()-k)];
+        arr[i] = temp[i];
     }
 }
 
 int main() {
     vector<int> arr = {1,2,3,4,5,6,7};
-    vector<int> arr2 = {1,2,3,4,5,6,7,8};
-    int k = 3;
+    vector<int> arr2 = {-1,-100,3,99};
+    int k = 2;
     rotateBrute(arr, k);
     for(int i : arr) {
         cout << i << " ";
@@ -36,4 +38,5 @@ int main() {
     for(int i : arr2) {
         cout << i << " ";
     }
+    
 }
