c8b16c80cb9cb3ed6a8cfaed6b20be93f5be9ef2
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-02-07 17:38:53 +0530

    Group Anagrams

---
+++ DSA-Series/6_Vectors_2/14_GroupAnagrams.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagramsBrute(vector<string> &strs) {
+    unordered_map<string, vector<string>> map;
+    for(auto i : strs) {
+        string word = i;
+        sort(word.begin(), word.end());
+        map[word].push_back(i);
+    }
+    vector<vector<string>> ans;
+    for(auto i : map) {
+        ans.push_back(i.second);
+    }
+    return ans;
+}
+
+int main() {
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+    vector<vector<string>> ans = groupAnagramsBrute(strs);
+    for(int i = 0; i < ans.size(); i++) {
+        for(int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << " ";
+        }
+    }
+}
