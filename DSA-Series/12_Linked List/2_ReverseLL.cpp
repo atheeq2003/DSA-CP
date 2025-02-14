2cd461cee25c92cb7b57a6162a8e8c12344a348f
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-02-14 21:13:40 +0530

    Reverse Linked List

---
+++ DSA-Series/12_Linked List/2_ReverseLL.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+
+    Node(int val, Node* next1) {
+        data = val;
+        next = next1;
+    }
+};
+
+class List {
+public:
+    Node* head;
+    Node* tail;
+ 
+    List() {
+        head = tail = nullptr;
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if(head == nullptr) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void print() {
+        Node* temp = head;
+        while(temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next; 
+        }
+        cout << endl;
+    }
+
+    Node* reverseList(Node* head) {
+        Node* prev = nullptr;
+        Node* curr = head;
+        Node* next = nullptr;
+        while(curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+};
+
+int main() {
+    List ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    
+    ll.head = ll.reverseList(ll.head);
+
+    ll.print();
+}
