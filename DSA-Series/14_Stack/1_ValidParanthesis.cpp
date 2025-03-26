b8bbf6b3ffa2b16c049931d1916e55738e246011
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-03-26 23:27:57 +0530

    Valid Paranthesis

---
+++ DSA-Series/14_Stack/1_ValidParanthesis.cpp
@@ -0,0 +1,34 @@
+// 20. Valid Parentheses -> https://leetcode.com/problems/valid-parentheses/description/
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string str) {
+    stack<char> st;
+    for(char i : str) {
+        if(i == '(' || i == '[' || i == '{') {
+            st.push(i);
+        }
+        else {
+            if(st.empty()) {
+                return false;
+            }
+            else {
+                if(st.top() == '(' && i == ')' || st.top() == '[' && i == ']' || st.top() == '{' && i == '}') {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string s1 = "()";
+    string s2 = "()[]{}";
+    string s3 = "(]";
+    string s4 = "([])";
+    cout << isValid(s4) << endl;
+}
