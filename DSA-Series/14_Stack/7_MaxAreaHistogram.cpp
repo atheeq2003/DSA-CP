48dcab610e5d7600e15dced702cd7cb3912c8fe5
atheeq_2003 <atheequrrahaman8@gmail.com>
2025-04-02 01:17:56 +0530

    Largest Rectangle in Histogram

---
+++ DSA-Series/14_Stack/7_MaxAreaHistogram.cpp
@@ -0,0 +1,55 @@
+// 84. Largest Rectangle in Histogram -> https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestRectangleArea(vector<int> &heights) {
+    int n = heights.size();
+    vector<int> right(n, 0);
+    vector<int> left(n, 0);
+    stack<int> s;
+    int ans = 0;
+    // right
+    for(int i = n-1; i >= 0; i--) {
+        while(s.size() > 0 && heights[s.top()] >= heights[i]) {
+            s.pop();
+        }
+
+        if(s.empty()) {
+            right[i] = n;
+        }
+        else {
+            right[i] = s.top();
+        }
+        s.push(i);
+    }
+
+    while(!s.empty()) {
+        s.pop();
+    }
+
+    for(int i = 0; i < n; i++) {
+        while(s.size() > 0 && heights[s.top()] >= heights[i]) {
+            s.pop();
+        }
+
+        if(s.empty()) {
+            left[i] = n;
+        }
+        else {
+            left[i] = s.top();
+        }
+        s.push(i);
+    }
+
+    for(int i = 0; i < n; i++) {
+        int width = right[i] - left[i] - 1;
+        int currArea = heights[i] * width;
+        ans = max(ans, currArea);
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> heights = {2,1,5,6,2,3};
+    cout << largestRectangleArea(heights);
+}
